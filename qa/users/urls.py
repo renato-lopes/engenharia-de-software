68a5175201749f66bedbc56609d355ac6c758cba
Deiziane <deiziane_ns@hotmail.com>
2019-04-06 17:10:04 -0300

    add register page + add urls

--- qa/users/urls.py
+++ qa/users/urls.py
@@ -1,7 +1,9 @@
 from django.contrib.auth import views as auth_views
 from django.urls import path, include
+from . import views
 
 
 urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('register/', views.register, name='register'),
 ] 
--- qa/users/views.py
+++ qa/users/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+def register(request):
+    context = {
+        'title': 'Registro'
+    }
+
+    return render(request, 'users/register.html', context)
