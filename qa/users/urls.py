1f16856bf11af77fb86d2baa173aec8bcfe61790
Deiziane <deiziane_ns@hotmail.com>
2019-04-06 18:31:05 -0300

    add pass reset done page

--- qa/users/urls.py
+++ qa/users/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('register/', views.register, name='register'),
     path('password_reset/', auth_views.PasswordResetView.as_view(template_name='users/password_reset.html'), name='password_reset'),
+    path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='users/password_reset_done.html'), name='password_reset_done'),
 ] 
