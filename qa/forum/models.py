3f549e7d9c6508edbaddbb957beca8540e27096d
brenoam <brenoam@dcc.ufmg.br>
2019-04-24 00:09:05 -0300

    Define forum models

--- qa/forum/models.py
+++ qa/forum/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Question(models.Model):
+	description = models.CharField(max_length=8192)
+	user = models.ForeignKey('users.User', on_delete=models.CASCADE)
+
+class Answer(models.Model):
+	description = models.CharField(max_length=8192)
+	user = models.ForeignKey('users.User', on_delete=models.CASCADE)
+	question = models.ForeignKey(Question, on_delete=models.CASCADE)
+
+class Tags(models.Model):
+	description = models.CharField(max_length=512)
+	
+class QuestionTags(models.Model):
+	question = models.ForeignKey(Question, on_delete=models.CASCADE)
+	tags = models.ForeignKey(Tags, on_delete=models.CASCADE)
--- qa/qa/settings.py
+++ qa/qa/settings.py
@@ -37,8 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'forum',
     'users.apps.UsersConfig',
+    'forum.apps.ForumConfig',
 ]
 
 MIDDLEWARE = [
