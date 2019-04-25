4462a66f01b207ca7f73e68f92a4ad7286fd2a0a
brenoam <brenoam@dcc.ufmg.br>
2019-04-24 22:59:07 -0300

    Add title do Question and dates to Question and Answer

--- qa/forum/models.py
+++ qa/forum/models.py
@@ -2,11 +2,16 @@ from django.db import models
 
 # Create your models here.
 class Question(models.Model):
-	description = models.CharField(max_length=8192)
+	title = description = models.CharField(max_length=512)
+	description = models.TextField()
+	creation_date = models.DateTimeField(auto_now=False, auto_now_add=True)
+	last_modification_date = models.DateTimeField(auto_now=True, auto_now_add=False)
 	user = models.ForeignKey('users.User', on_delete=models.CASCADE)
 
 class Answer(models.Model):
-	description = models.CharField(max_length=8192)
+	description = models.TextField()
+	creation_date = models.DateTimeField(auto_now=False, auto_now_add=True)
+	last_modification_date = models.DateTimeField(auto_now=True, auto_now_add=False)
 	user = models.ForeignKey('users.User', on_delete=models.CASCADE)
 	question = models.ForeignKey(Question, on_delete=models.CASCADE)
 
@@ -15,4 +20,4 @@ class Tags(models.Model):
 	
 class QuestionTags(models.Model):
 	question = models.ForeignKey(Question, on_delete=models.CASCADE)
-	tags = models.ForeignKey(Tags, on_delete=models.CASCADE)
+	tags = models.ForeignKey(Tags, on_delete=models.CASCADE)	
