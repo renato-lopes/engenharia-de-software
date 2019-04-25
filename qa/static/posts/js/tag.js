e948f7480aa5c71b3d516017586ac12f8072c683
Deiziane <deiziane_ns@hotmail.com>
2019-04-25 18:10:05 -0300

    change tag input

--- qa/static/posts/js/tag.js
+++ qa/static/posts/js/tag.js
@@ -1,74 +1,48 @@
 // Code based in: https://codepen.io/srph/pen/rxoLMG?editors=0010
 
-var number_of_tags = 5
-var tags = [];
-var $container = document.querySelector('.tag-field');
-var $input = document.querySelector('input');
-var $tags = document.querySelector('.js-tags');
+function existingTag(text)
+{
+	var existing = false,
+		text = text.toLowerCase();
 
-$container.addEventListener('click', function() {
-  $input.focus();
-});
+	$(".tags").each(function(){
+		if ($(this).text().toLowerCase() == text) 
+		{
+			existing = true;
+			return "";
+		}
+	});
 
-$container.addEventListener('keydown', function(evt) {
-  if ( !evt.target.matches('.js-tag-input') ) {
-    return;
-  }
-  
-  if ( evt.keyCode !== 13 ) {
-    return;
-  }
-  
-  var value = String(evt.target.value);
-  
-  if ( !value.length || value.length > 20 || tags.length === number_of_tags ) {
-    return;
-  }
-  
-  tags.push(evt.target.value);
-  $input.value = '';
-  render(tags, $tags);
-});
-
-$container.addEventListener('keydown', function(evt) {
-  if ( !evt.target.matches('.js-tag-input') ) {
-    return;
-  }
-  
-  if ( evt.keyCode !== 8 ) {
-    return;
-  }
-  
-  if ( String(evt.target.value).length ) {
-    return;
-  }
-  
-  tags = tags.slice(0, tags.length - 1);
-  $input.value = '';
-  render(tags, $tags);
-});
-
-$container.addEventListener('click', function(evt) {
-  if ( evt.target.matches('.js-tag-close') || evt.target.matches('.js-tag') ) {
-    tags = tags.filter(function(tag, i) {
-      return i != evt.target.getAttribute('data-index');
-    });
-    render(tags, $tags);
-  }
-}, true);
- 
-
-function render(tags, el) {
-console.log(tags)
-  el.innerHTML = tags.map(function(tag, i) {
-    return (
-      '<div class="tag js-tag" data-index="' + i + '">' +
-        tag +
-        '<span class="tag-close js-tag-close" data-index="' + i + '">×</span>' +
-      '</div>'
-   );
-  }).join('') + (tags.length === number_of_tags ? '' : '<input placeholder="Adicione tags..." class="js-tag-input">')
-  ;
-  
-  $container.querySelector('.js-tag-input').focus();
+	return existing;
 }
+
+$(function(){
+  $(".tags-new input").focus();
+  
+  $(".tags-new input").keyup(function(){
+
+		var tag = $(this).val().trim(),
+		length = tag.length;
+
+		if((tag.charAt(length - 1) == ',') && (tag != ","))
+		{
+			tag = tag.substring(0, length - 1);
+
+			if(!existingTag(tag))
+			{
+				$('<li class="tags"><span>' + tag + '</span><i class="fa fa-times"></i></i></li>').insertBefore($(".tags-new"));
+				$(this).val("");	
+			}
+			else
+			{
+				$(this).val(tag);
+			}
+		}
+	});
+  
+  $(document).on("click", ".tags i", function(){
+    $(this).parent("li").remove();
+  });
+
+});
+                                
