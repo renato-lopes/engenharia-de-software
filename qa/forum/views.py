7f8bd459c4c90b20ff362974d9e071cfa84b8253
brenoam <brenoam@dcc.ufmg.br>
2019-04-25 00:39:30 -0300

    Create new questions

--- qa/forum/views.py
+++ qa/forum/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 
+from .models import *
+
 # Create your views here.
 def index(request):
     context = {
@@ -13,3 +15,21 @@ def users(request):
         'title': 'Usuários'
     }
     return render(request, 'forum/users.html', context)
+
+def create_question(request):
+    context = {
+        'title': 'Nova Pergunta'
+    }
+    try:
+        # Get data from request
+        title = request.POST['title']
+        description = request.POST['description']
+    except KeyError:
+        return render(request, 'forum/create_question.html', context)
+    else:
+        #TODO: Missing User
+        
+        new_question = Question(title=title, description=description)
+        new_question.save()
+        context['confirm_message'] = "Question {} sucessfully created!".format(title)
+        return render(request, 'forum/create_question.html', context)
