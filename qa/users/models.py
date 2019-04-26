468bb1cd42f8bf4ea5a81c7faf1e829029a31961
Deiziane <deiziane_ns@hotmail.com>
2019-04-26 18:48:35 -0300

    duno

--- qa/users/models.py
+++ qa/users/models.py
@@ -1,9 +1,2 @@
 from django.db import models
-
-class User(models.Model):
-    username = models.CharField(max_length=200)
-    email = models.EmailField(max_length=200)
-    password = models.CharField(max_length=50)
-
-    def __str__(self):
-        return self.username
+from django.contrib.auth.models import User
