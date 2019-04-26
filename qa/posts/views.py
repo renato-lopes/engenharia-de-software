73caf73dc519f777ae44a3a458fcff603a6fdb6d
brenoam <brenoam@dcc.ufmg.br>
2019-04-26 01:11:56 -0300

    Create question

--- qa/posts/views.py
+++ qa/posts/views.py
@@ -6,16 +6,17 @@ def create_post(request):
     context = {
         'title': 'Nova Pergunta'
     }
+    if not request.user.is_authenticated:
+        return render(request, 'users/login.html', context)
     try:
         # Get data from request
         title = request.POST['title']
         description = request.POST['description']
+        tags = request.POST['tags']
     except KeyError:
         return render(request, 'posts/create_post.html', context)
     else:
-        #TODO: Missing User
-        
-        new_question = Question(title=title, description=description)
+        new_question = Question(title=title, description=description, user=request.user)
         new_question.save()
         context['confirm_message'] = "Post {} sucessfully created!".format(title)
         return render(request, 'posts/create_post.html', context)
