282aa3db2189569273a1dbedcd6b9285d397f661
brenoam <brenoam@dcc.ufmg.br>
2019-05-31 02:24:23 -0300

    Questions refactoring

--- posts/models.py
+++ posts/models.py
@@ -13,6 +13,38 @@ class Question(models.Model):
 	upvote = models.ManyToManyField(AuthUser, related_name="upvote", blank=True)
 	downvote = models.ManyToManyField(AuthUser, related_name="downvote", blank=True)
 
+	def create(title, description, tags, user):
+		question = Question(title=title, description=description, user=user)
+		question.save()
+		QuestionTag.assign_tags_to_questions(question, tags)
+		return question
+
+	def search(**kwargs):
+		result = Question.objects.filter(**kwargs)
+		questions = []
+		for el in result:
+			questions.append(Question.get(el.id))
+		return questions
+
+	def read(id):
+		try:
+			question = Question.objects.get(id= id)
+		except:
+			question = None
+		return question
+
+	def update(id, title, description, tags):
+		question = Question.read(id)
+		question.title = title
+		question.description = description
+		question.save()
+		QuestionTag.assign_tags_to_questions(question, tags)
+		return question
+
+	def remove(id):
+		question = Question.read(id)
+		question.delete()
+
 	def get_absolute_url(self):
 		return reverse("posts:post", kwargs={"id_post": self.id})
 
@@ -31,11 +63,67 @@ class Answer(models.Model):
 	def get_question(self):
 		return self.question
 
-
-
 class Tag(models.Model):
 	name = models.CharField(max_length=512)
+
+	def create(name):
+		tag = Tag(name=name)
+		tag.save()
+		return tag
+
+	def read(id):
+		try:
+			tag = Tag.objects.get(id= id)
+		except:
+			tag = None
+		return tag
+
+	def search(**kwargs):
+		tags = []
+		result = Tag.objects.filter(**kwargs)
+		for el in result:
+			tags.append(Tag.objects.get(id= el.id))
+		return tags
+
+	def update(name):
+		tag = Tag.read(id)
+		tag.name = name
+		tag.save()
+		return tag
 	
+	def delete(id):
+		tag = Tag.read(id)
+		tag.delete()
+
 class QuestionTag(models.Model):
 	question = models.ForeignKey(Question, on_delete=models.CASCADE)
-	tag = models.ForeignKey(Tag, on_delete=models.CASCADE)	
+	tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
+
+	def create(question, tag):
+		question_tag = QuestionTag(question=question, tag=tag)
+		question_tag.save()
+		return tag
+
+	def assign_tags_to_questions(question, tags):
+		question_tags = QuestionTag.search(question=question)
+		for qt in question_tags:
+			qt.delete()
+		
+		for tag_name in tags.split(','):
+			if(len(tag_name) == 0):
+				continue
+			tag_name = tag_name.strip()
+			tag_res = Tag.search(name=tag_name)
+			if (len(tag_res) == 0):
+				tag = Tag.create(name=tag_name)
+			else:
+				tag = tag_res[0]
+			QuestionTag.create(question, tag);
+			
+
+	def	search(**kwargs):
+		question_tags = []
+		result = QuestionTag.objects.filter(**kwargs)
+		for el in result:
+			question_tags.append(QuestionTag.objects.get(id=el.id))
+		return question_tags
--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.views.generic import RedirectView
+from django.http import HttpResponse, HttpResponseNotFound
 
 from users.models import ForumUser as User
 
@@ -20,30 +21,17 @@ def create_post(request):
     else:
         username = request.user.username
         user = User.objects.get(username=username)
-        new_question = Question(title=title, description=description, user=user)
-        new_question.save()
-        for tag_name in tags.split(','):
-            if(len(tag_name) == 0):
-                continue
-            tag_name = tag_name.strip()
-            tag_res = Tag.objects.filter(name=tag_name)
-            if (len(tag_res) == 0):
-                new_tag = Tag(name=tag_name)
-                new_tag.save()
-            else:
-                new_tag = tag_res[0]
-            new_question_tag = QuestionTag(question=new_question, tag=new_tag)
-            new_question_tag.save()
+        new_question = Question.create(title=title, description=description, tags=tags, user=user)
         return redirect("/post/"+str(new_question.id))
 
 def post(request,id_post):
 
-    question = get_object_or_404(Question, pk=id_post) #questao clicada pelo usuario
-    
-    question_tags = QuestionTag.objects.filter(question=id_post)
-    tags = []
-    for el in question_tags:
-        tags.append(el.tag)
+    question = Question.read(id_post)
+    if (question == None):
+        return HttpResponseNotFound('<h1>Pergunta '+str(id_post)+' não encontrada</h1>')
+
+    question_tags = QuestionTag.search(question = id_post)
+    tags = [Tag.read(qt.tag.id) for qt in question_tags]
     
     answers = Answer.objects.filter(question=id_post).order_by("creation_date")
 
@@ -146,12 +134,12 @@ class AnswerDislikeToggle(RedirectView):
 
 
 def edit_post(request, post_id):
-    question = get_object_or_404(Question, pk=post_id)
+    question = Question.read(post_id)
+    if (question == None):
+        return HttpResponseNotFound('<h1>Pergunta '+str(post_id)+' não encontrada</h1>')
     
-    question_tags = QuestionTag.objects.filter(question=post_id)
-    tags = []
-    for el in question_tags:
-        tags.append(el.tag.name)
+    question_tags = QuestionTag.search(question = post_id)
+    tags = [Tag.read(qt.tag.id).name for qt in question_tags]
         
     context = {
         'title': 'Editar pergunta',
@@ -173,34 +161,16 @@ def edit_post(request, post_id):
     except (KeyError):
         return render(request, 'posts/edit_post.html', context)
     else:
-        question.title = title
-        question.description = description
-        question.save()
-        
-        QuestionTag.objects.filter(question=post_id).delete()
-        
-        for tag_name in tags.split(','):
-            if(len(tag_name) == 0):
-                continue
-            print (tag_name)
-            tag_name = tag_name.strip()
-            tag_res = Tag.objects.filter(name=tag_name)
-            if (len(tag_res) == 0):
-                new_tag = Tag(name=tag_name)
-                new_tag.save()
-            else:
-                new_tag = tag_res[0]
-            new_question_tag = QuestionTag(question=question, tag=new_tag)
-            new_question_tag.save()
+        Question.update(id=post_id, title=title, description=description, tags=tags)
         return redirect("/post/"+str(question.id))
 
 def delete_post(request, post_id):
-    question = get_object_or_404(Question, pk=post_id) #questao clicada pelo usuario
-    
-    question_tags = QuestionTag.objects.filter(question=post_id)
-    tags = []
-    for el in question_tags:
-        tags.append(el.tag.name)
+    question = Question.read(post_id)
+    if (question == None):
+        return HttpResponseNotFound('<h1>Pergunta '+str(post_id)+' não encontrada</h1>')
+
+    question_tags = QuestionTag.search(question = post_id)
+    tags = [Tag.read(qt.tag.id).name for qt in question_tags]
     
     answers = Answer.objects.filter(question=post_id).order_by("creation_date")
     
@@ -223,18 +193,15 @@ def delete_post(request, post_id):
     except (KeyError):
         return render(request, 'posts/delete_post.html', context)
     else:
-        question.delete()
+        Question.remove(post_id)
         return redirect("/all-posts")
 
 def tag(request,tag_id):
-    tag = get_object_or_404(Tag, pk=tag_id)
-    # tag = Tag.objects.get(id=tag_id)
-    print(tag.name)
-    
-    question_tags = QuestionTag.objects.filter(tag=tag_id)
-    questions = []
-    for el in question_tags:
-        questions.append(Question.objects.get(id=el.question.id))
+    tag = Tag.read(post_id)
+    if (tag == None):
+        return HttpResponseNotFound('<h1>Tag '+str(tag_id)+' não encontrada</h1>')
+    question_tags = QuestionTag.search(tag=tag_id)
+    questions = [Question.read(qt.question.id) for qt in question_tags]
     
     context = {
         "questions": questions,
@@ -246,11 +213,10 @@ def tag(request,tag_id):
 
 def edit_answer(request,answer_id):
     answer = get_object_or_404(Answer, pk=answer_id)
-    question = Question.objects.get(id=answer.question.id)
-    question_tags = QuestionTag.objects.filter(question=question.id)
-    tags = []
-    for el in question_tags:
-        tags.append(el.tag.name)
+    # question = Question.objects.get(id=answer.question.id)
+    question = Question.read(answer.question.id)
+    question_tags = QuestionTag.search(question = post_id)
+    tags = [Tag.read(qt.tag.id).name for qt in question_tags]
     
     
     context = {
@@ -278,12 +244,10 @@ def edit_answer(request,answer_id):
         
 def delete_answer(request,answer_id):
     answer = get_object_or_404(Answer, pk=answer_id)
-    question = Question.objects.get(id=answer.question.id)
-    question_tags = QuestionTag.objects.filter(question=question.id)
-    tags = []
-    for el in question_tags:
-        tags.append(el.tag.name)
-    
+    # question = Question.objects.get(id=answer.question.id)
+    question = Question.read(answer.question.id)
+    question_tags = QuestionTag.search(question = post_id)
+    tags = [Tag.read(qt.tag.id).name for qt in question_tags]
     
     context = {
         "title": "Excluir resposta",
